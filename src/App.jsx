04e9a79bdeda51891c3c5101b9abcdac32680d03
Marcela Mancini <marcelamancinidiaz@gmail.com>
2025-10-29 02:12:49 -0300

    Fix: eliminar importación TeAssistNotice no usada

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import Proyectos from './pages/Proyectos';
 import CV from './pages/CV';
 import Contacto from './pages/Contacto';
-import TeAssistNotice from './pages/TeAssistNotice';
 import AdminCV from "./pages/AdminCV";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Route path="/proyectos" element={<Proyectos />} />
         <Route path="/cv" element={<CV />} />
         <Route path="/contacto" element={<Contacto />} />
-        <Route path="/teassist" element={<TeAssistNotice />} />
         <Route path="/admin" element={<AdminCV />} />
       </Routes>
     </Router>
