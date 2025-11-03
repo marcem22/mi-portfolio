e273c996330acb68b47a2ce4d67ab484613813e7
Marcela Mancini <marcelamancinidiaz@gmail.com>
2025-11-03 12:27:31 -0300

    dfs

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
+console.log("🔑 Firebase key:", import.meta.env.VITE_FIREBASE_API_KEY);
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
